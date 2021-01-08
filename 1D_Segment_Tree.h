a2ecda1cc41d44feadb07f77b9bc4d0867e0348f
Sarthak Saha <51643611+sAr0603@users.noreply.github.com>
2021-01-09 02:33:54 +0530

    Update 1D_Segment_Tree.h

--- 1D_Segment_Tree.h
+++ 1D_Segment_Tree.h
@@ -1,12 +1,8 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-#define all(x) (x).begin(), (x).end()
-#define isOdd(x) ((x)&1)
+#include "myLibrary.h"
 
 /*Making them global to enable maximum size possible -> Part of Iterative segNode class*/
-array<int, (int) (2 * 2 * 10e5)> segTree; // 1 - index based segTree
-array<int, (int) (2 * 2 * 10e5)> __lazy;//Change size of segTree by getting proper value from segSize(int n) fn
+array<int, 4000000> segTree; // 1 - index based segTree
+array<int, 4000000> __lazy;//Change size of segTree by getting proper value from segSize(int n) fn
 /*IF YOU COMMENT OUT __lazy then fns depending on it will throw compiler errors*/
 
 
@@ -36,7 +32,7 @@ class segNode {
           qAns = Q(qAns, segTree[l++]);//Hence operate on lth node at present level only
         if (isOdd(r))
           qAns = Q(qAns, segTree[--r]);//same -> take example of 8 sized array
-        l = __parentOf(l), r = __parentOf(r);//float up the range to corresponding parents
+        l = parentOf(l), r = parentOf(r);//float up the range to corresponding parents
       }
       return qAns;
     }
@@ -45,8 +41,8 @@ class segNode {
     void pointUpdate(int pos, int value) {
       segTree[pos += n] = value; // change value at desired leaf node
       while (pos > 1) {//have hit root
-        pos = __parentOf(pos);//moving upto parent
-        segTree[pos] = Q(segTree[__rightOf(pos)], segTree[__leftOf(pos)]);//recompute till root
+        pos = parentOf(pos);//moving upto parent
+        segTree[pos] = Q(segTree[leftOf(pos)], segTree[rightOf(pos)]);//recompute till root
       }
     }
 
@@ -60,7 +56,7 @@ public:
       fill(all(segTree), EMPTY);
     }//static constructor
 
-    void build(vector<int> &arr) {
+    void build(v<int> &arr) {
       fill(all(segTree), EMPTY);
       fill(all(__lazy), 0);//comment it out if not using lazy
       n = arr.size();
@@ -70,24 +66,24 @@ public:
 
 /*/---------------------------backend----------------------/*/
 private:
-    void __build_tree(vector<int> &arr) {
+    void __build_tree(v<int> &arr) {
       for (int i = 0; i < n; i++)
         segTree[n + i] = arr[i];//leaf nodes of segTree are same as all elements of the array
 
       for (int i = n - 1; i >= 1; i--)
-        segTree[i] = Q(segTree[__leftOf(i)], segTree[__rightOf(i)]);//computing internal nodes :
+        segTree[i] = Q(segTree[rightOf(i)], segTree[leftOf(i)]);//computing internal nodes :
       //Q[ROOT] = Q[ Q[LST] + Q[RST] ]
     }//DONT TOUCH
 
-    static inline int __leftOf(int &index) {
+    static inline int rightOf(int index) {
       return (index << 1);
     }//NEVER TOUCH
 
-    static inline int __rightOf(int &index) {
+    static inline int leftOf(int index) {
       return (index << 1) + 1;
     }//NEVER TOUCH
 
-    static inline int __parentOf(int &index) {
+    static inline int parentOf(int index) {
       if (index == 1)
         cerr << "You already reached parent dumbfuck" << endl;
       return index >> 1;
@@ -127,10 +123,10 @@ public: // frontend, edit according to requirements
 /*/---------------------------recursive-lazy----------------------/*/
 
 class SEG_NODE {//0->indexed
-    vector<int> seg_tree;
-    vector<int> lazy;
-    vector<int> &arr;
-    unsigned int n;
+    v<int> seg_tree;
+    v<int> lazy;
+    v<int> &arr;
+    int n;
 
 /*/---------------------------Identity&Operation----------------------/*/
     const int EMPTY = numeric_limits<int>::max(); //Identity element
@@ -141,7 +137,7 @@ class SEG_NODE {//0->indexed
 /*/---------------------------Identity&Operation----------------------/*/
 
 public:
-    explicit SEG_NODE(vector<int> &arr) : arr(arr), n(arr.size()) {
+    explicit SEG_NODE(v<int> &arr) : arr(arr), n(arr.size()) {
       auto tree_height = 2 * closest_exp2(n) - 1;
       seg_tree.assign(tree_height, EMPTY);
       lazy.assign(tree_height, 0);
@@ -155,15 +151,15 @@ public:
     void print() {
       for (auto &i : seg_tree) {
         if (i == EMPTY)
-          cout << "\\0" << '\t';
+          cerr << "\\0" << '\t';
         else
-          cout << i << '\t';
+          cerr << i << '\t';
       }
-      cout << endl;
+      cerr << endl;
     }
 
     void update(int pos, int value) {//point update
-      pointUpdate(pos, value, 0, n - 1, 0);
+      point_update(pos, value, 0, n - 1, 0);
     }
 
     int lazy_query(int l, int r) {
@@ -174,10 +170,13 @@ public:
       lazy_update(l, r, value, 0, n - 1, 0);
     }
 
+    void flush_lazy() {
+      flush_lazy(0, n - 1, 0, [](int x) { deb(x); });
+    }
+
 private:
-    template<typename UnsignedType>
-    UnsignedType closest_exp2(UnsignedType v) {
-      static_assert(std::is_unsigned<UnsignedType>::value, "Only works for unsigned types");
+
+    static int closest_exp2(uint v) {
       v--;
       for (size_t i = 1;
            i < sizeof(v) * CHAR_BIT; i *= 2) //Prefer size_t "Warning comparison between signed and unsigned integer"
@@ -185,16 +184,25 @@ private:
       return ++v;
     }
 
-    static inline int __leftOf(int &index) {
+    static inline int leftOf(int &index) {
       return (index << 1) + 1;
     }//NEVER TOUCH
 
-    static inline int __rightOf(int &index) {
+    static inline int rightOf(int index) {
       return (index << 1) + 2;
     }//NEVER TOUCH
 
     static inline bool isLeaf(int start, int end) {
       return start == end;
+    }
+
+    void resolve_lazy(int start, int end, int index) {
+      seg_tree[index] += lazy[index];//resolving lazy value
+      if (!isLeaf(start, end)) {// propagating any previous lazy value while visiting
+        lazy[leftOf(index)] += lazy[index];
+        lazy[rightOf(index)] += lazy[index];
+      }//we are sure that we wont any gross laziness to the tree hence this much resolve is enough
+      lazy[index] = 0;//lazy value resolved
     }
 
     void build_tree(int start, int end, int index) {
@@ -205,10 +213,10 @@ private:
       int mid = start + (end - start) / 2;
       /*first reaching the base case
       by calling recursively before any operations to reach base case of trivial queries*/
-      build_tree(start, mid, __leftOf(index));
-      build_tree(mid + 1, end, __rightOf(index));//post-order
+      build_tree(start, mid, leftOf(index));
+      build_tree(mid + 1, end, rightOf(index));//post-order
       /*Q[ROOT] = Q[ Q[LST] + Q[RST] ]*/
-      seg_tree[index] = Q(seg_tree[__leftOf(index)], seg_tree[__rightOf(index)]);
+      seg_tree[index] = Q(seg_tree[leftOf(index)], seg_tree[rightOf(index)]);
     }
 
     int query(int l, int r, int start, int end, int index) {
@@ -217,12 +225,12 @@ private:
       if (r < start || l > end)
         return EMPTY;//CASE III -> NO overlap
       int mid = start + (end - start) / 2;
-      int left_query = query(l, r, start, mid, __leftOf(index));//CASE II -> Partial overlap
-      int right_query = query(l, r, mid + 1, end, __rightOf(index));
+      int left_query = query(l, r, start, mid, leftOf(index));//CASE II -> Partial overlap
+      int right_query = query(l, r, mid + 1, end, rightOf(index));
       return Q(left_query, right_query);
     }
 
-    void pointUpdate(int pos, int value, int start, int end, int index) {
+    void point_update(int pos, int value, int start, int end, int index) {
       if (pos < start || pos > end)
         return;//CASE III -> NO overlap
       if (isLeaf(start, end)) {
@@ -230,54 +238,56 @@ private:
         return;
       }
       int mid = start + (end - start) / 2;
-      pointUpdate(pos, value, start, mid, __leftOf(index));//CASE II
-      pointUpdate(pos, value, mid + 1, end, __rightOf(index));
-      seg_tree[index] = Q(seg_tree[__leftOf(index)], seg_tree[__rightOf(index)]);
+      point_update(pos, value, start, mid, leftOf(index));//CASE II
+      point_update(pos, value, mid + 1, end, rightOf(index));
+      seg_tree[index] = Q(seg_tree[leftOf(index)], seg_tree[rightOf(index)]);
     }//point update helper
 
     int lazy_query(int l, int r, int start, int end, int index) {
-      seg_tree[index] += lazy[index];//resolving lazy value
-      if (!isLeaf(start, end)) {// propagating any previous lazy value while visiting
-        lazy[__leftOf(index)] += lazy[index];
-        lazy[__rightOf(index)] += lazy[index];
-      }//we are sure that we wont any gross laziness to the tree hence this much resolve is enough
-      lazy[index] = 0;//lazy value resolved
+      resolve_lazy(start, end, index);
       if (l <= start && r >= end)
         return seg_tree[index];//CASE I
       if (r < start || l > end)
         return EMPTY;//CASE III
       int mid = start + (end - start) / 2;
-      int left_query = lazy_query(l, r, start, mid, __leftOf(index));//CASE II
-      int right_query = lazy_query(l, r, mid + 1, end, __rightOf(index));
+      int left_query = lazy_query(l, r, start, mid, leftOf(index));//CASE II
+      int right_query = lazy_query(l, r, mid + 1, end, rightOf(index));
       return Q(left_query, right_query);
     }
 
     void lazy_update(int l, int r, int value, int start, int end, int index) {
-      seg_tree[index] += lazy[index];//resolving lazy value//lazy value resolved
-      if (!isLeaf(start, end)) {// propagating any previous lazy value while visiting unless we hit leaf node
-        lazy[__leftOf(index)] += lazy[index];
-        lazy[__rightOf(index)] += lazy[index];
-      }
-      lazy[index] = 0;//lazy value value at present node resolved
+      resolve_lazy(start, end, index);
 
       if (r < start || l > end)//CASE III No overlap
         return;
 
       if (l <= start && r >= end) {//CASE I Complete overlap
-        seg_tree[index] += value;//UPDATE
-        if (!isLeaf(start, end)) {//non-leaf node
-          lazy[__leftOf(index)] += value;//postponing updates as lazy value to descendants
-          lazy[__rightOf(index)] += value;//Saving O(K) calls ; k = r-l
-        }
+        resolve_lazy(start, end, index);
         return;//lazy optimization , baad mai karenge , no resolving k nodes where k = l-r
       }
 
       int mid = start + (end - start) / 2;
-      lazy_update(l, r, value, start, mid, __leftOf(index));//CASE II Partial overlap
-      lazy_update(l, r, value, mid + 1, end, __rightOf(index));
-      seg_tree[index] = Q(seg_tree[__leftOf(index)], seg_tree[__rightOf(index)]);
+      lazy_update(l, r, value, start, mid, leftOf(index));//CASE II Partial overlap
+      lazy_update(l, r, value, mid + 1, end, rightOf(index));
+      seg_tree[index] = Q(seg_tree[leftOf(index)], seg_tree[rightOf(index)]);
     }
-}; //Recursive Segment Tree with lazy propagation
 
+    template<typename f>
+    void flush_lazy(int start, int end, int index, const f &visit = [](int x) {}) {
+      resolve_lazy(start, end, index);
+      if (isLeaf(start, end)) {
+        visit(seg_tree[index]);
+        return;
+      }
+      int mid = start + (end - start) / 2;
+      /*first reaching the base case
+      by calling recursively before any operations to reach base case of trivial queries*/
+      flush_lazy(start, mid, leftOf(index), visit);
+      flush_lazy(mid + 1, end, rightOf(index), visit);//post-order
+      /*Q[ROOT] = Q[ Q[LST] + Q[RST] ]*/
+      seg_tree[index] = Q(seg_tree[leftOf(index)], seg_tree[rightOf(index)]);
+    }
+
+}; //Recursive Segment Tree with lazy propagation
 
 /*/---------------------------recursive-lazy----------------------/*/
